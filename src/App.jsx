5721491e4bdc78191b1907397dd0503d6cab2d78
Fahad ghaffar <fahad.ghaffar97@gmail.com>
2024-08-02 04:08:44 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,35 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import studentGroup from "./assets/Images/pexels-kampus-5940827.jpg";
+
+import GlobalButton from "./components/Button";
 function App() {
   return (
     <div className="w-full h-[500px] bg-[url(./assets/Images/pexels-kampus-5940827.jpg)] bg-no-repeat bg-center bg-cover">
       {/* <div className='w-full h-full  bg-red-600/30 backdrop-brightness-75'> */}
       <div className="w-full h-full bg-gradient-to-r  from-red-900/100 to-red-600/30 backdrop-brightness-75 ">
         <div className="container h-full flex justify-around  mx-auto items-center ">
-          <div className="w-full h-full">
-            <h1 className="text-white text-5xl font-normal font-Bebas_Neue">
+          <div className="w-full h-full flex flex-col justify-center pl-5 pr-5">
+
+            <h1 className="text-white text-6xl font-normal font-Bebas_Neue mb-3">
               Your Trusted Student <br />
               Storage Solution
             </h1>
 
-            <p className="text-white font-Lato_Regular text-xl font-normal">
+            <p className="text-white font-Lato_Regular text-xl font-normal mb-6">
               At Campus Keep Storage, we understand the challenges students face
               when it comes to moving out for the summer. That’s why we offer a
               seamless and stress-free solution for storing your belongings
               during the break.
             </p>
 
-            <div className="flex ">
-              <button>Transform Your Look!</button>
-              <button>Call Us Today!</button>
-            </div>
+            <div>
+             <GlobalButton name="Sign up today"/>
+             </div>
+          
           </div>
-          <div className="w-full h-full">
-          <div> world</div>
+          <div className="w-full h-full flex justify-center items-center">
+          <div className="flex w-3/4 h-3/4 bg-white rounded-xl w-11/12 "> world</div>
           </div>
         </div>
       </div>
---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const GlobalButton = ({name}) => {
+  return (
+    <div className='flex items-center  '>
+    
+    <p className='text-white font-normal font-Lato_Regular text-sm uppercase bg-[#56ACDE] px-11 py-4'>{name}</p>
+
+    </div>
+  )
+}
+
+export default GlobalButton
