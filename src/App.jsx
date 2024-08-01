0041aeed43046a55403f96d7622746d5260c3244
Fahad Ghaffar <fahad.ghaffar@lslogics.local>
2024-08-01 21:28:30 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import studentGroup from "./assets/Images/pexels-kampus-5940827.jpg";
 function App() {
-
-
   return (
-      <div className='w-full h-[500px] bg-slate-700  ' >
-            <div className='container h-full flex justify-evenly  mx-auto bg-white '>
-                        <div> 
-                          <h2>ENHANCE</h2>
-                          <h1>Your Beauty</h1>
+    <div className="w-full h-[500px] bg-[url(./assets/Images/pexels-kampus-5940827.jpg)] bg-no-repeat bg-center bg-cover">
+      {/* <div className='w-full h-full  bg-red-600/30 backdrop-brightness-75'> */}
+      <div className="w-full h-full bg-gradient-to-r  from-red-900/100 to-red-600/30 backdrop-brightness-75 ">
+        <div className="container h-full flex justify-around  mx-auto items-center ">
+          <div>
+            <h1 className="text-white text-5xl font-normal font-Bebas_Neue">
+              Your Trusted Student <br />
+              Storage Solution
+            </h1>
 
-                          <p>Achieve the Look You Desire
-                          with Safe and Professional Treatments</p>
-                          
-                          <div className='flex '>
-                            <button>Transform Your Look!</button>
-                            <button>Call Us Today!</button>
-                            
-                            </div>
-                          </div>
-                        <div> world</div>
+            <p className="text-white font-Lato_Regular text-xl font-normal">
+              At Campus Keep Storage, we understand the challenges students face
+              when it comes to moving out for the summer. That’s why we offer a
+              seamless and stress-free solution for storing your belongings
+              during the break.
+            </p>
+
+            <div className="flex ">
+              <button>Transform Your Look!</button>
+              <button>Call Us Today!</button>
             </div>
-
+          </div>
+          <div> world</div>
+        </div>
       </div>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'studentGroup' : "url('/src/assets/images/pexels-kampus-5940827.jpg')",
+       
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        Bebas_Neue: ["Bebas_Neue", "sans-serif"],
+        Lato_Regular: ["Lato_Regular", "sans-serif"],
+
+      },
+    },
   },
   plugins: [],
 }
